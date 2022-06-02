80e78fce7d07c24a559314bf75be710b8826ac2e
ezedin <ezedin38nuru@gmail.com>
2022-06-02 05:40:43 -0700

    00

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *
+ * a program that prints "Programming is like building a multilingual puzzle"
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	puts("\""Programming is like building a multilingual puzzle");
+
+	return 0;
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *
+ * with proper grammar, but the outcome is a piece of art,
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return 0;
+
+}
