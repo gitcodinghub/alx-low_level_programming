3dca84d33370da0dfaf0c8effb52ef8bf75afd43
gitcodinghub <104759635+gitcodinghub@users.noreply.github.com>
2022-06-03 14:47:01 +0300

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,20 +7,18 @@
  */
 int main(void)
 {
-	char c;
-
-	c = 'a';
-	while (c <= 'z')
-	{
-		if ((c != 'e') && (c != 'q'))
-		{
-			putchar(c);
-			++c;
-		}
-		else
-			++c;
-	}
-	putchar('\n');
-
-	return (0);
+char c;
+c = 'a';
+while (c <= 'z')
+{
+if ((c != 'e') && (c != 'q'))
+{
+putchar(c);
+++c;
+}
+else
+++c;
+}
+putchar('\n');
+return (0);
 }
