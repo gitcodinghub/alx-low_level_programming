ac9ebddf801cb0e16bbe7c6bcebfe7fd9ec3e3b4
ezedin <ezedin38nuru@gmail.com>
2022-06-03 12:57:52 -0700

    a

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,14 +3,14 @@
 /**
  * main - Entry point
  *
- * Return: returns 0 when everything works fine
+ * Return: returns 0 when everything fine
  *
  */
+
 int main(void)
 {
-
 	int n;
-	
+
 	for (n = 122; n >= 97; --n)
 	{
 		putchar(n);
