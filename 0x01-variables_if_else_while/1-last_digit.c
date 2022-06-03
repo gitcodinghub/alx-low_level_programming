2afc963aa42567d6aa8632b0f0cd6149c35b6be6
ezedin <ezedin38nuru@gmail.com>
2022-06-03 09:34:17 -0700

    00

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,13 @@
 #include <stdio.h>
 
 /**
-*main - Entry point
-*Return: Always 0 (Success)
-*/
-
+ * *main - Entry point
+ * *Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	
@@ -28,6 +27,5 @@ int main(void)
 	{
 		printf("less than 6 and not 0\n");
 	}
-	
 	return (0);
 }
