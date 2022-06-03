8bcda80f1403c10e96de8d7769ab568f78dac69c
ezedin <ezedin38nuru@gmail.com>
2022-06-03 10:42:02 -0700

    0

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main -Entry point
  *
  * Return: returns 0 when everything works fine
  *
  */
-
 int main(void)
 {
 	int n;
@@ -14,7 +13,8 @@ int main(void)
 	for (n = 97; n <= 122; ++n)
 		putchar(n);
 	for (n = 65; n <= 90; ++n)
-		putchar(n);
+	       putchar(n);
 	putchar(10);
 	return (0);
 }
+
