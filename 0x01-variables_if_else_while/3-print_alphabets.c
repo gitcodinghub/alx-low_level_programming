1b6b418a3d0c76356162dc045d772dd0991bb62d
gitcodinghub <104759635+gitcodinghub@users.noreply.github.com>
2022-06-03 13:19:33 +0300

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+* main- Entry point
+*
+* Return: Always 0 (Success)
+*/
+int  main(void)
 {
-	char low;
+	int x;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-	for (low = 'A'; low <= 'Z'; low++)
-		putchar(low);
-		putchar('\n');
-
+	for (x = 0; x < 26; x++)
+	{
+		c = 97 + x;
+		putchar(c);
+	}
+	for (x = 0; x < 26; x++)
+	{
+		c = 65 + x;
+		putchar(c);
+	}
+	putchar('\n');
 	return (0);
 }
