feb1a47bb620a6a5b0c864bc5a6a7cb4e398f8a2
ezedin <ezedin38nuru@gmail.com>
2022-06-07 10:56:18 -0700

    0

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,12 @@
  */
 void print_alphabet_x10 (void)
 {
-	for (int i = 0; i < 10; i++)
+	int i;
+	char j;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (char j = 97; j <= 122; j++i)
+		for (j = 97; j <= 122; j++)
 			_putchar(j);
 		_putchar('\n');
 	}
