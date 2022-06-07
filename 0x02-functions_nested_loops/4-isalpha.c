0016639581ace0d79736f98ce75ee5e29aa75454
ezedin <ezedin38nuru@gmail.com>
2022-06-07 04:08:30 -0700

    0

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,15 +1,16 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - entry point
+ * _isalpha - letter or not
  *
- * Description: Prints 
+ * 
  *
- * Return: 0
+ * Return: nothing
  */
-int main(void)
+int _isalpha(int c)
 {
-	return (0);
+	if(c >= 97 && c <= 122 || c >= 65 && c <= 90)
+		return 1;
+	else
+		return 0;
 }
