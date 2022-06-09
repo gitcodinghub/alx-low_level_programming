6454fb12b5e9b03fba1f97144dc771a54463c04d
ezedin <ezedin38nuru@gmail.com>
2022-06-09 10:39:54 -0700

    0

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,10 @@
 
 /**
  * main - prints the numbers from 1 to 100
- * Return: Always 0 Success
+ * 3 multiples print Fizz instead of the number
+ * 5 multiples print Buzz instead of the number
+ * 3 & 5 multiples print FizzBuzz instead of the number
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
