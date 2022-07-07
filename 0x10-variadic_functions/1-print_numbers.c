89c6d7c0cfa711b121b2b001cc41bd20a4afe0b5
ezedin <ezedin38nuru@gmail.com>
2022-07-07 13:41:05 -0700

    -

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+*print_numbers - print  arguments of numbers
+*@separator: separator between numbers (string)
+*@n: number of past numbers
+*@...: must be integers
+*
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	/* initialize valist*/
+	va_start(valist, n);
+	/* access each argument of valist */
+	for (i = 0; i < n; i++)
+	{
+	if (i != 0 && separator != NULL)
+	{
+		printf("%s", separator);
+	}
+	printf("%d", va_arg(valist, int));
+	}
+	printf("\n");
+	/* clean memory reserved*/
+	va_end(valist);
+}
